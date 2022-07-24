67c1aad025ea334ac6bad18f997e14c6a329cd04
EvVlF <ev.vl.fedorov@gmail.com>
2022-07-24 21:58:09 +0400

    Ah... Done. Updated with Google style code

--- src/com/EvDroid/Calculation.java
+++ src/com/EvDroid/Calculation.java
@@ -7,79 +7,88 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 
 class Calculation {
-    private static final ArrayList<StringBuilder> reversedPolishNotation;
+  private static final ArrayList<StringBuilder> reversedPolishNotation;
 
-    static {
-        reversedPolishNotation = new ArrayList<>(Parsing.getParsedInPolishNotation());
-    }
+  static {
+    reversedPolishNotation = new ArrayList<>(Parsing.getParsedInPolishNotation());
+  }
 
-    public static void main(String[] args) throws NotCorrectExpression {
-        System.out.println("Результат: " + getCalculation());
-    }
+  public static void main(String[] args) throws NotCorrectExpression {
+    System.out.println("Результат: " + getCalculation());
+  }
 
-    private static double calculation() throws NotCorrectExpression {
-        for (int i = 0; reversedPolishNotation.size() != 1; i++) {
-            if (Calculation.reversedPolishNotation.size() == 2 || reversedPolishNotation.size() == 0) {
-                throw new NotCorrectExpression("Неверное выражение");
-            }
-            if (Character.isDigit(reversedPolishNotation.get(i).charAt(0))) {
-                continue;
-            } else {
-                StringBuilder result = new StringBuilder();
-                switch (reversedPolishNotation.get(i).charAt(0)) {
-                    case '+':
-                        result = add(reversedPolishNotation.get(i - 2), reversedPolishNotation.get(i - 1));
-                        reversedPolishNotation.set(i, result);
-                        reversedPolishNotation.removeAll(reversedPolishNotation.subList(i - 2, i));
-                        i = 0;
-                        break;
-                    case '-':
-                        result = subtract(reversedPolishNotation.get(i - 2), reversedPolishNotation.get(i - 1));
-                        reversedPolishNotation.set(i, result);
-                        reversedPolishNotation.removeAll(reversedPolishNotation.subList(i - 2, i));
-                        i = 0;
-                        break;
-                    case '*':
-                        result = multiply(reversedPolishNotation.get(i - 2), reversedPolishNotation.get(i - 1));
-                        reversedPolishNotation.set(i, result);
-                        reversedPolishNotation.removeAll(reversedPolishNotation.subList(i - 2, i));
-                        i = 0;
-                        break;
-                    case '/':
-                        result = divide(reversedPolishNotation.get(i - 2), reversedPolishNotation.get(i - 1));
-                        reversedPolishNotation.set(i, result);
-                        reversedPolishNotation.removeAll(reversedPolishNotation.subList(i - 2, i));
-                        i = 0;
-                        break;
-                    default:
-                        throw new RuntimeException("Неверное выражение");
-                }
-            }
+  private static double calculation() throws NotCorrectExpression {
+    for (int i = 0; reversedPolishNotation.size() != 1; i++) {
+      if (Calculation.reversedPolishNotation.size() == 2 || reversedPolishNotation.size() == 0) {
+        throw new NotCorrectExpression("Неверное выражение");
+      }
+      if (Character.isDigit(reversedPolishNotation.get(i).charAt(0))) {
+        continue;
+      } else {
+        StringBuilder result = new StringBuilder();
+        switch (reversedPolishNotation.get(i).charAt(0)) {
+          case '+':
+            result = add(reversedPolishNotation.get(i - 2), reversedPolishNotation.get(i - 1));
+            reversedPolishNotation.set(i, result);
+            reversedPolishNotation.removeAll(reversedPolishNotation.subList(i - 2, i));
+            i = 0;
+            break;
+          case '-':
+            result = subtract(reversedPolishNotation.get(i - 2), reversedPolishNotation.get(i - 1));
+            reversedPolishNotation.set(i, result);
+            reversedPolishNotation.removeAll(reversedPolishNotation.subList(i - 2, i));
+            i = 0;
+            break;
+          case '*':
+            result = multiply(reversedPolishNotation.get(i - 2), reversedPolishNotation.get(i - 1));
+            reversedPolishNotation.set(i, result);
+            reversedPolishNotation.removeAll(reversedPolishNotation.subList(i - 2, i));
+            i = 0;
+            break;
+          case '/':
+            result = divide(reversedPolishNotation.get(i - 2), reversedPolishNotation.get(i - 1));
+            reversedPolishNotation.set(i, result);
+            reversedPolishNotation.removeAll(reversedPolishNotation.subList(i - 2, i));
+            i = 0;
+            break;
+          default:
+            throw new RuntimeException("Неверное выражение");
         }
-        return Double.parseDouble(reversedPolishNotation.get(0).toString());
+      }
     }
+    return Double.parseDouble(reversedPolishNotation.get(0).toString());
+  }
 
-    private static @NotNull StringBuilder add(@NotNull StringBuilder operand1, @NotNull StringBuilder operand2) {
-        return new StringBuilder().append(new BigDecimal(operand1.toString()).add(new BigDecimal(operand2.toString())));
-    }
+  private static @NotNull StringBuilder add(
+      @NotNull StringBuilder operand1, @NotNull StringBuilder operand2) {
+    return new StringBuilder()
+        .append(new BigDecimal(operand1.toString()).add(new BigDecimal(operand2.toString())));
+  }
 
-    private static @NotNull StringBuilder subtract(@NotNull StringBuilder operand1, @NotNull StringBuilder operand2) {
-        return new StringBuilder().append(new BigDecimal(operand1.toString()).subtract(new BigDecimal(operand2.toString())));
-    }
+  private static @NotNull StringBuilder subtract(
+      @NotNull StringBuilder operand1, @NotNull StringBuilder operand2) {
+    return new StringBuilder()
+        .append(new BigDecimal(operand1.toString()).subtract(new BigDecimal(operand2.toString())));
+  }
 
-    private static @NotNull StringBuilder multiply(@NotNull StringBuilder operand1, @NotNull StringBuilder operand2) {
-        return new StringBuilder().append(new BigDecimal(operand1.toString()).multiply(new BigDecimal(operand2.toString())));
-    }
+  private static @NotNull StringBuilder multiply(
+      @NotNull StringBuilder operand1, @NotNull StringBuilder operand2) {
+    return new StringBuilder()
+        .append(new BigDecimal(operand1.toString()).multiply(new BigDecimal(operand2.toString())));
+  }
 
-    @SuppressWarnings("BigDecimalMethodWithoutRoundingCalled")
-    private static @NotNull StringBuilder divide(@NotNull StringBuilder operand1, @NotNull StringBuilder operand2) throws NotCorrectExpression {
-        if (operand2.toString().equals("0")) {
-            throw new NotCorrectExpression("Деление на ноль");
-        }
-        return new StringBuilder().append(new BigDecimal(operand1.toString()).divide(new BigDecimal(operand2.toString())));
+  @SuppressWarnings("BigDecimalMethodWithoutRoundingCalled")
+  private static @NotNull StringBuilder divide(
+      @NotNull StringBuilder operand1, @NotNull StringBuilder operand2)
+      throws NotCorrectExpression {
+    if (operand2.toString().equals("0")) {
+      throw new NotCorrectExpression("Деление на ноль");
     }
+    return new StringBuilder()
+        .append(new BigDecimal(operand1.toString()).divide(new BigDecimal(operand2.toString())));
+  }
 
-    public static double getCalculation() throws NotCorrectExpression {
-        return calculation();
-    }
+  public static double getCalculation() throws NotCorrectExpression {
+    return calculation();
+  }
 }
--- src/com/EvDroid/Parsing.java
+++ src/com/EvDroid/Parsing.java
@@ -8,52 +8,58 @@ import java.util.ArrayDeque;
 import java.util.Objects;
 
 class Parsing {
-    private static final ArrayDeque<StringBuilder> validatedExpression;
+  private static final ArrayDeque<StringBuilder> validatedExpression;
 
-    static {
-        try {
-            validatedExpression = Validation.getValidatedExpression();
-        } catch (NotCorrectExpression e) {
-            throw new RuntimeException(e);
-        }
+  static {
+    try {
+      validatedExpression = Validation.getValidatedExpression();
+    } catch (NotCorrectExpression e) {
+      throw new RuntimeException(e);
     }
+  }
 
-    private static final ArrayDeque<StringBuilder> asStack = new ArrayDeque<>();
-    private static final ArrayDeque<StringBuilder> preReversedPolishNotation = new ArrayDeque<>();
+  private static final ArrayDeque<StringBuilder> asStack = new ArrayDeque<>();
+  private static final ArrayDeque<StringBuilder> preReversedPolishNotation = new ArrayDeque<>();
 
-    @SuppressWarnings("SameReturnValue")
-    private static ArrayDeque<StringBuilder> preParseInPolishNotation() {
-        while (!validatedExpression.isEmpty()) {
-            StringBuilder elementOfValidatedExpression = validatedExpression.pop();
-            char charAt0 = elementOfValidatedExpression.charAt(0);
-            if (Character.isDigit(charAt0)) {
-                preReversedPolishNotation.add(elementOfValidatedExpression);
-            } else {
-                switch (charAt0) {
-                    case '+':
-                    case '-':
-                        while (!asStack.isEmpty() && ('+' == asStack.peek().charAt(0) || '+' == Objects.requireNonNull(asStack.peek()).charAt(0) || '*' == Objects.requireNonNull(asStack.peek()).charAt(0) || '/' == (Objects.requireNonNull(asStack.peek())).charAt(0))) {
-                            preReversedPolishNotation.add(asStack.pop());
-                        }
-                        asStack.push(elementOfValidatedExpression);
-                        break;
-                    case '*':
-                    case '/':
-                        while (!asStack.isEmpty() && ('*' == asStack.peek().charAt(0) || '/' == (Objects.requireNonNull(asStack.peek())).charAt(0))) {
-                            preReversedPolishNotation.add(asStack.pop());
-                        }
-                        asStack.push(elementOfValidatedExpression);
-                        break;
-                }
+  @SuppressWarnings("SameReturnValue")
+  private static ArrayDeque<StringBuilder> preParseInPolishNotation() {
+    while (!validatedExpression.isEmpty()) {
+      StringBuilder elementOfValidatedExpression = validatedExpression.pop();
+      char charAt0 = elementOfValidatedExpression.charAt(0);
+      if (Character.isDigit(charAt0)) {
+        preReversedPolishNotation.add(elementOfValidatedExpression);
+      } else {
+        switch (charAt0) {
+          case '+':
+          case '-':
+            while (!asStack.isEmpty()
+                && ('+' == asStack.peek().charAt(0)
+                    || '+' == Objects.requireNonNull(asStack.peek()).charAt(0)
+                    || '*' == Objects.requireNonNull(asStack.peek()).charAt(0)
+                    || '/' == (Objects.requireNonNull(asStack.peek())).charAt(0))) {
+              preReversedPolishNotation.add(asStack.pop());
             }
+            asStack.push(elementOfValidatedExpression);
+            break;
+          case '*':
+          case '/':
+            while (!asStack.isEmpty()
+                && ('*' == asStack.peek().charAt(0)
+                    || '/' == (Objects.requireNonNull(asStack.peek())).charAt(0))) {
+              preReversedPolishNotation.add(asStack.pop());
+            }
+            asStack.push(elementOfValidatedExpression);
+            break;
         }
-        while (!asStack.isEmpty()) {
-            preReversedPolishNotation.add(asStack.pop());
-        }
-        return preReversedPolishNotation;
+      }
     }
+    while (!asStack.isEmpty()) {
+      preReversedPolishNotation.add(asStack.pop());
+    }
+    return preReversedPolishNotation;
+  }
 
-    public static @NotNull @Unmodifiable ArrayDeque<StringBuilder> getParsedInPolishNotation() {
-        return preParseInPolishNotation();
-    }
+  public static @NotNull @Unmodifiable ArrayDeque<StringBuilder> getParsedInPolishNotation() {
+    return preParseInPolishNotation();
+  }
 }
